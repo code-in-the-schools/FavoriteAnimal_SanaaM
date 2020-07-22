948823f90a0142a8a91ad319ff5916cd5ca4fce2
Sanaa514 <sanaamontgomery10@gmail.com>
2020-07-22 15:25:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#Facts about my favorite animal
+
+num = int(input("Enter a number: "))
+if num == 1:  
+  print("They have two stomachs,one is for storage and the other is for digestion.")
+elif num == 2: 
+  print("They sleep with only hair their brain.")
+elif num == 3:
+  print("They use specific individual whistles in order to identify each other.")
+elif num == 4:
+  print("They use their teeth, not ears, to pick up the echoes that bounce back from echolocation.")
+else: 
+  print("Invalid number")
